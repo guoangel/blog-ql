1bc791ccae0048f9e147945a16409376a78db0fb
angel <fsguoangel@hotmail.com>
2022-03-09 00:38:53 +0800

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use App\User;
+use App\Topic;
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 20)->create();
+        Topic::create(['name' => 'Featured Sites', 'slug' => 'featured']);
+        Topic::create(['name' => 'Useful Links', 'slug' => 'links']);
+        Topic::create(['name' => 'Guides & Tutorials', 'slug' => 'tutorials']);
+
+        factory(Post::class, 20)->create();
+    }
+}
